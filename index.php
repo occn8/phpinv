d5c80afdb394248c18ea8850bbe0356eba25c8ea
occn8 <ochenwayward@gmail.com>
2020-08-23 15:24:46 +0300

    index

--- index.php
+++ index.php
@@ -79,9 +79,9 @@
     </div>
 
     <div class="pricing-header px-3 py-3 pt-md-2 pb-md-4 mx-auto text-center">
-        <p class="lead">U-invent is a suite of open source business apps that cover all your company needs: CRM, eCommerce, 
+        <p class="lead">"U-invent is a suite of open source business apps that cover all your company needs: CRM, eCommerce, 
             accounting, inventory, point of sale, project management, etc.
-            U-invent's unique value proposition is to be at the same time very easy to use and fully integrated.</p>
+            U-invent's unique value proposition is to be at the same time very easy to use and fully integrated."</p>
     </div>
 
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
